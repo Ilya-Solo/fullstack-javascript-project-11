d7a56b6d6be7b248926d7da7e4205aa9b2fd2da9
Ilya Salanovich <ilyasolonovich20@gmail.com>
2024-05-26 21:12:22 +0200

    add basic form validation

--- src/index.js
+++ src/index.js
@@ -1,19 +1,51 @@
 import  './styles.scss';
 import  'bootstrap';
-const yup = require('yup');
+import onChange from 'on-change';
+import * as yup from 'yup';
 
-const form = document.querySelector('.rss-form')
+
+const form = document.querySelector('.rss-form');
+const input = form.querySelector('input');
+const errorMessageEl = document.querySelector('.feedback');
+
+const state = {
+    form: {
+        isValid: true,
+        errorMessage: '',
+    },
+    uiState: {}
+}
+
+const renderForm = (state) => {
+    input.classList.remove('is-invalid');
+    errorMessageEl.classList.remove('text-danger');
+    errorMessageEl.textContent = '';
+    if(!state.form.isValid) {
+        input.classList.add('is-invalid');
+        errorMessageEl.classList.add('text-danger');
+        errorMessageEl.textContent = state.form.errorMessage;
+    }
+}
+
+const watchedState = onChange(state, (path, value, previousValue) => {
+    if (path === "form.isValid" && value !== previousValue) {
+        renderForm(watchedState);
+    }
+    
+})
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const data = new FormData(e.target);
     const urlSchema = yup.string().url();
     const validUrl = data.get('url')
     urlSchema.validate(validUrl)
-        .then(isValid => {
-            console.log(isValid);
+        .then(() => {
+            watchedState.form.errorMessage = '';
+            watchedState.form.isValid = true;
         })
         .catch(error => {
-            console.error('Validation error:', error.message);
+            watchedState.form.errorMessage = error.message;
+            watchedState.form.isValid = false;
         });
     // console.log(data.get('url'));
 })
