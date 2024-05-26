f6b77b313d430e1ea71f911fc993394e95a412d6
Ilya Salanovich <ilyasolonovich20@gmail.com>
2024-05-26 13:54:42 +0200

    start form validation step

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import  './styles.scss';
 import  'bootstrap';
-console.log("Hello World!");
+const yup = require('yup');
+
+const form = document.querySelector('.rss-form')
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const urlSchema = yup.string().url();
+    const validUrl = data.get('url')
+    urlSchema.validate(validUrl)
+        .then(isValid => {
+            console.log(isValid);
+        })
+        .catch(error => {
+            console.error('Validation error:', error.message);
+        });
+    // console.log(data.get('url'));
+})
